e06baf6cd3a3e372580110f45b4cb68522e44489
m-ghiringhelli <mghiring@gmail.com>
2022-03-08 16:29:33 -0800

    trying to fix ci issues

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header/Header';
-import Main from './components/Main/Main.js';
-import Footer from './components/Footer/Footer.js';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
